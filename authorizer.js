173253dfc3693038e6e66ddc865c16dfcd1bdcd1
Michael Holloway <michaeldavidholloway@gmail.com>
2014-12-28 20:56:36 -0500

    no need to delete db rows; removed stray spaces

--- authorizer.js
+++ authorizer.js
@@ -15,8 +15,8 @@ var credentials = {key: privKey, cert: certificate};
 var app = express();
 var httpsServer = https.createServer(credentials, app);
 
-var client_id = 'MY-CLIENT-ID'; 
-var client_secret = 'MY-CLIENT-SECRET'; 
+var client_id = 'MY-CLIENT-ID';
+var client_secret = 'MY-CLIENT-SECRET';
 var callback_redirect_uri = 'MY-CALLBACK-URI';
 
 var pg = require('pg');
@@ -27,23 +27,23 @@ var access_token;
 var refresh_token;
 var deviceId;
 
-var updateDatabase = function(deviceId){	
+var updateDatabase = function(deviceId){
   pg.connect(conString, function(err, client, done){
 	client.query('SELECT device_id FROM tokens WHERE device_id = \'' + deviceId + '\'', function(err, result){
       // add new row if device id not in database
       if (result.rows[0] == undefined || result.rows[0].device_id != deviceId) {
 	    console.log('Could not find device ' + deviceId + ' in db.  Adding new row.' + '\n');
-        client.query('INSERT INTO tokens VALUES (\'' + deviceId + '\', \'' + refresh_token 
+        client.query('INSERT INTO tokens VALUES (\'' + deviceId + '\', \'' + refresh_token
 			+ '\', CURRENT_TIMESTAMP)', function(err, result){
 	      if(err){
 		    return console.error('error inserting new row for device id ' + deviceId, err);
 	      }
-	      console.log('ADDING NEW row for device id ' + deviceId + ' with refresh token ' + refresh_token + '\n');		  
+	      console.log('ADDING NEW row for device id ' + deviceId + ' with refresh token ' + refresh_token + '\n');
 		  done();
 		}); // end of inner client.query block #1
       } else if (result.rows[0].device_id == deviceId){
         console.log('Found device ' + deviceId + ' in database!');
-        client.query('UPDATE tokens SET refresh_token = \'' + refresh_token 
+        client.query('UPDATE tokens SET refresh_token = \'' + refresh_token
 		    + '\', timestamp = CURRENT_TIMESTAMP WHERE device_id = \'' + deviceId + '\'', function(err, result){
 		  if(err){
 		    return console.error('error updating row for device id ' + deviceId, err);
@@ -59,7 +59,7 @@ var updateDatabase = function(deviceId){
 app.use(express.static(__dirname + '/public'))
 
 app.get('/callback', function(req, res) {
-	
+
   deviceId = req.query.uid;
   var code = req.query.code || null;
 
@@ -88,18 +88,18 @@ app.get('/callback', function(req, res) {
 	  // send access token to Android app
 	  res.send(access_token);
 	  console.log('New access token ' + access_token + ' sent!' + '\n');
-	
-	  updateDatabase(deviceId);	
+
+	  updateDatabase(deviceId);
 	} else {
       res.send('invalid token!');
     }
-  }); 
+  });
 });
 
 app.get('/refresh_token', function(req, res) {
-	
+
   deviceId = req.query.uid;
-  
+
   // get refresh token from db if it exists, otherwise send back null response
   pg.connect(conString, function(err, client, done){
   	if (err) {
@@ -114,7 +114,7 @@ app.get('/refresh_token', function(req, res) {
 	  } else {
 		refresh_token = result.rows[0].refresh_token;
 		console.log('FOUND refresh token ' + refresh_token + '\n');
-		
+
 		//if refresh token exists, send to Spotify
 	    var authOptions = {
 	      url: 'https://accounts.spotify.com/api/token',
@@ -138,30 +138,6 @@ app.get('/refresh_token', function(req, res) {
 	  done();
     }); //end of client.query block
   });  //end of client.connect block
-}); 
-
-app.get('/delete', function(req, res) {
-	
-  deviceId = req.query.uid;
-
-  // get refresh token from db if it exists, otherwise send back null response
-  pg.connect(conString, function(err, client, done){
-  	if (err) {
-  	  return console.error('could not connect to postgres', err);
-    }
-    client.query('DELETE FROM tokens WHERE device_id = \'' + deviceId + '\'', function(err, result) {
-      if (err) {
-        return console.error('error running query', err);
-      }
-      if (result == 'DELETE 1') {
-		console.log('DELETED record for device id ' + deviceId);
-		res.send('OK');
-	  } else {
-		console.log('DELETE for device id ' + deviceId + ' failed!');
-      }
-      done();
-    }); //end of client.query block
-  }); //end of client.connect block
 });
 
 httpsServer.listen(8443);
